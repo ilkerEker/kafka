3568498cc6d38afc0bcc3e1afb176278f7b9475a
ilkerEker <lkrkr@hotmail.com>
2022-07-22 15:57:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace kafka
+{
+    public class Program
+    {
+        static void  Main(string[] args)
+        {
+            
+            process();
+        }
+        
+        public async static void process()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "10.1.10.36:9092",
+                ClientId = Dns.GetHostName(),
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                string text = null;
+
+                while (text!="quit")
+                {
+                    Console.Write("Add Message");
+                    text=Console.ReadLine();
+                    producer.ProduceAsync("message",new Message<Null, string> { Value = text });
+                    //Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+
+
+                }   
+                producer.Flush(new TimeSpan(100));
+            }
+        }
+            
+        }
+
+    }
